79a3ac342c3ef35ea2f271004e02f574690a840f
Chris R <tratcher@outlook.com>
2017-11-14 07:04:26 -0800

    Enable automatic migrations

--- FindItApiService/Migrations/Configuration.cs
+++ FindItApiService/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace FindItApiService.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(FindItApiService.Models.FindItApiContext context)
